2f5e9cc00c58a6ea1f6064d2dcabadd1b216adfc
Benjamin Höglinger <nefarius@dhmx.at>
2016-11-18 18:43:43 +0100

    Added HidGuardian

---
+++ HidGuardian/Driver.h
@@ -0,0 +1,36 @@
+/*++
+
+Module Name:
+
+    driver.h
+
+Abstract:
+
+    This file contains the driver definitions.
+
+Environment:
+
+    Kernel-mode Driver Framework
+
+--*/
+
+#define INITGUID
+
+#include <ntddk.h>
+#include <wdf.h>
+
+#include "device.h"
+#include "queue.h"
+#include "trace.h"
+
+EXTERN_C_START
+
+//
+// WDFDRIVER Events
+//
+
+DRIVER_INITIALIZE DriverEntry;
+EVT_WDF_DRIVER_DEVICE_ADD HidGuardianEvtDeviceAdd;
+EVT_WDF_OBJECT_CONTEXT_CLEANUP HidGuardianEvtDriverContextCleanup;
+
+EXTERN_C_END
---
+++ HidGuardian/Public.h
@@ -0,0 +1,24 @@
+/*++
+
+Module Name:
+
+    public.h
+
+Abstract:
+
+    This module contains the common declarations shared by driver
+    and user applications.
+
+Environment:
+
+    user and kernel
+
+--*/
+
+//
+// Define an Interface Guid so that app can find the device and talk to it.
+//
+
+DEFINE_GUID (GUID_DEVINTERFACE_HidGuardian,
+    0x2f6dcf26,0xeb50,0x44ea,0x90,0x93,0x4d,0x83,0x5c,0xdd,0xef,0x2f);
+// {2f6dcf26-eb50-44ea-9093-4d835cddef2f}

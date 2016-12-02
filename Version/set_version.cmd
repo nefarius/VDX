f4a453b7ad73cf32f47e4785d4148cea29509df4
Benjamin Höglinger <nefarius@dhmx.at>
2016-12-02 19:30:41 +0100

    Added version helper utility

---
+++ Version/set_version.cmd
@@ -0,0 +1,10 @@
+@echo off
+SET mypath=%~dp0
+SET solution=%mypath:~0,-1%\..
+
+honestus --version-from-file version.txt --target-file "%solution%\HidCerberus.Lib\Properties\AssemblyInfo.cs" --assembly.version --assembly.file-version
+honestus --version-from-file version.txt --target-file "%solution%\HidCerberus.Srv\Properties\AssemblyInfo.cs" --assembly.version --assembly.file-version
+honestus --version-from-file version.txt --target-file "%solution%\HidGuardian\HidGuardian.rc" --resource.file-version --resource.product-version
+honestus --version-from-file version.txt --target-file "%solution%\HidGuardian\HidGuardian.vcxproj" --vcxproj.inf-time-stamp
+honestus --version-from-file version.txt --target-file "%solution%\ViGEmBus\ViGEmBus.rc" --resource.file-version --resource.product-version
+honestus --version-from-file version.txt --target-file "%solution%\ViGEmBus\ViGEmBus.vcxproj" --vcxproj.inf-time-stamp

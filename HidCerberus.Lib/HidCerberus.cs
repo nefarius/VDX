f9ee76d3e1a7dddf943742f2bc260ffac2029f62
Benjamin Höglinger <nefarius@dhmx.at>
2016-12-10 15:02:09 +0100

    Implemented IDisposable

--- HidCerberus.Lib/HidCerberus.cs
+++ HidCerberus.Lib/HidCerberus.cs
@@ -8,7 +8,7 @@ using RGiesecke.DllExport;
 
 namespace HidCerberus.Lib
 {
-    public class HidCerberus
+    public class HidCerberus : IDisposable
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private static readonly IHidCerberusWcf ServiceChannel;
@@ -54,6 +54,41 @@ namespace HidCerberus.Lib
         {
             ServiceChannel?.RemovePid(Process.GetCurrentProcess().Id);
         }
+
+        #region IDisposable Support
+        private bool _disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposedValue) return;
+
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects).
+                HidGuardianClose();
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+            // TODO: set large fields to null.
+
+            _disposedValue = true;
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~HidCerberus() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
 
         #endregion
     }

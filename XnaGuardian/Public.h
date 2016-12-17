9a8e54c96ab78e53a58fbae0fae65c0b9195f540
Benjamin Höglinger <nefarius@dhmx.at>
2016-12-17 20:04:47 +0100

    Duplicated HidGuardian into XnaGuardian project

---
+++ XnaGuardian/Public.h
@@ -0,0 +1,32 @@
+/*
+MIT License
+
+Copyright (c) 2016 Benjamin "Nefarius" Höglinger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+//
+// Define an Interface Guid so that app can find the device and talk to it.
+//
+
+DEFINE_GUID (GUID_DEVINTERFACE_XnaGuardian,
+    0x2f6dcf26,0xeb50,0x44ea,0x90,0x93,0x4d,0x83,0x5c,0xdd,0xef,0x2f);
+// {2f6dcf26-eb50-44ea-9093-4d835cddef2f}

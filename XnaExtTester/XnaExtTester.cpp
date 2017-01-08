1d1d2301f8ecdd7f1d11bce8fb2c40e30370a545
Benjamin Höglinger <nefarius@dhmx.at>
2017-01-08 19:00:22 +0100

    Added missing license headers

--- XnaExtTester/XnaExtTester.cpp
+++ XnaExtTester/XnaExtTester.cpp
@@ -11,8 +11,8 @@ int main()
     pad.wButtons |= XINPUT_GAMEPAD_A;
     pad.wButtons |= XINPUT_GAMEPAD_Y;
 
-    printf("%X\n", XInputOverrideSetMask(0, XINPUT_GAMEPAD_OVERRIDE_A | XINPUT_GAMEPAD_OVERRIDE_Y));
-    printf("%X\n", XInputOverrideSetState(0, &pad));
+    XInputOverrideSetMask(0, XINPUT_GAMEPAD_OVERRIDE_A | XINPUT_GAMEPAD_OVERRIDE_Y);
+    XInputOverrideSetState(0, &pad);
 
     return 0;
 }

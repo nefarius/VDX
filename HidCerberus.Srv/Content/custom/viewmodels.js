d2bcd58d58521c1f6714498d05c0d7414e365f43
Benjamin Höglinger <nefarius@dhmx.at>
2017-05-01 20:43:48 +0200

    Moved JavaScript to its own files

---
+++ HidCerberus.Srv/Content/custom/viewmodels.js
@@ -0,0 +1,8 @@
+﻿/* Publish list of available devices */
+$.getJSON("/v1/hid/devices/get", function (data) {
+    function HidDevicesViewModel() {
+        var self = this;
+        self.hidDevices = ko.observableArray(data);
+    }
+    ko.applyBindings(new HidDevicesViewModel());
+});

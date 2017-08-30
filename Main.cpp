a9d1067be0270e99978fb3ce46c8d6f79f9f2524
Benjamin Höglinger <nefarius@dhmx.at>
2017-08-30 10:33:02 +0200

    Implementing ViGEmClient logic

--- Main.cpp
+++ Main.cpp
@@ -7,16 +7,33 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
-#include <SFML/Graphics/CircleShape.hpp>
 
+#include <Xinput.h>
 #include <ViGEmClient.h>
 
 
 int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "ViGEm: Dracula X");
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ViGEm: Dracula X", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
+
+    auto client = vigem_alloc();
+
+    PVIGEM_TARGET targets[XUSER_MAX_COUNT];
+
+    for (auto i = 0; i < _countof(targets); i++)
+    {
+        targets[i] = vigem_target_x360_alloc();
+    }
+
+    auto xInputMod = LoadLibrary(L"XInput1_3.dll");
+
+    auto pXInputEnable = reinterpret_cast<VOID(WINAPI*)(BOOL)>(GetProcAddress(xInputMod, "XInputEnable"));
+    auto pXInputGetState = reinterpret_cast<DWORD(WINAPI*)(DWORD, XINPUT_STATE*)>(GetProcAddress(xInputMod, "XInputGetState"));
+
+    vigem_connect(client);
+    pXInputEnable(TRUE);
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -40,6 +57,9 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine
         ImGui::SFML::Render(window);
         window.display();
     }
+
+    vigem_disconnect(client);
+    vigem_free(client);
 
     ImGui::SFML::Shutdown();
 }

df1350d20424c2452185c23738fecbc76bab5969
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2018-09-30 12:37:50 +0200

    Implemented version stamping through build script

--- build/Build.cs
+++ build/Build.cs
@@ -1,10 +1,15 @@
 using System;
+using System.IO;
 using System.Linq;
+using JsonConfig;
 using Nuke.Common;
+using Nuke.Common.BuildServers;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.MSBuild;
 using RunProcessAsTask;
+using Vestris.ResourceLib;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
@@ -14,8 +19,9 @@ class Build : NukeBuild
 {
     public static int Main () => Execute<Build>(x => x.Compile);
 
-    [Solution] readonly Solution Solution;
+    [Solution("VDX.sln")] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] private readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
@@ -32,7 +38,7 @@ class Build : NukeBuild
         .Executes(() =>
         {
             MSBuild(s => s
-                .SetTargetPath(SolutionFile)
+                .SetTargetPath(Solution)
                 .SetTargets("Restore"));
         });
 
@@ -64,7 +70,7 @@ class Build : NukeBuild
             // Build x64
             // 
             MSBuild(s => s
-                .SetTargetPath(SolutionFile)
+                .SetTargetPath(Solution)
                 .SetTargets("Rebuild")
                 .SetConfiguration(Configuration)
                 .SetMaxCpuCount(Environment.ProcessorCount)
@@ -75,12 +81,26 @@ class Build : NukeBuild
             // Build x86
             // 
             MSBuild(s => s
-                .SetTargetPath(SolutionFile)
+                .SetTargetPath(Solution)
                 .SetTargets("Rebuild")
                 .SetConfiguration(Configuration)
                 .SetMaxCpuCount(Environment.ProcessorCount)
                 .SetNodeReuse(IsLocalBuild)
                 .SetTargetPlatform(MSBuildTargetPlatform.x86));
+
+            if (Configuration.Equals("release", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var version =
+                    new Version(IsLocalBuild ? GitVersion.GetNormalizedFileVersion() : AppVeyor.Instance.BuildVersion);
+
+                StampVersion(
+                    Path.Combine(RootDirectory, @"bin\x64\VDX.exe"),
+                    version);
+
+                StampVersion(
+                    Path.Combine(RootDirectory, @"bin\x86\VDX.exe"),
+                    version);
+            }
         });
 
     private Target Pack => _ => _
@@ -88,10 +108,44 @@ class Build : NukeBuild
         .Executes(() =>
         {
             MSBuild(s => s
-                .SetTargetPath(SolutionFile)
+                .SetTargetPath(Solution)
                 .SetTargets("Restore", "Pack")
                 .SetPackageOutputPath(ArtifactsDirectory)
                 .SetConfiguration(Configuration)
                 .EnableIncludeSymbols());
         });
+
+    private static void StampVersion(string path, Version version)
+    {
+        var versionResource = new VersionResource
+        {
+            FileVersion = version.ToString(),
+            ProductVersion = version.ToString()
+        };
+
+        var stringFileInfo = new StringFileInfo();
+        versionResource[stringFileInfo.Key] = stringFileInfo;
+        var stringFileInfoStrings = new StringTable
+        {
+            LanguageID = 1033,
+            CodePage = 1200
+        };
+        stringFileInfo.Strings.Add(stringFileInfoStrings.Key, stringFileInfoStrings);
+        stringFileInfoStrings["CompanyName"] = Config.Global.Version.CompanyName;
+        stringFileInfoStrings["FileDescription"] = Config.Global.Version.FileDescription;
+        stringFileInfoStrings["FileVersion"] = version.ToString();
+        stringFileInfoStrings["InternalName"] = Config.Global.Version.InternalName;
+        stringFileInfoStrings["LegalCopyright"] = Config.Global.Version.LegalCopyright;
+        stringFileInfoStrings["OriginalFilename"] = Config.Global.Version.OriginalFilename;
+        stringFileInfoStrings["ProductName"] = Config.Global.Version.ProductName;
+        stringFileInfoStrings["ProductVersion"] = version.ToString();
+
+        var varFileInfo = new VarFileInfo();
+        versionResource[varFileInfo.Key] = varFileInfo;
+        var varFileInfoTranslation = new VarTable("Translation");
+        varFileInfo.Vars.Add(varFileInfoTranslation.Key, varFileInfoTranslation);
+        varFileInfoTranslation[ResourceUtil.USENGLISHLANGID] = 1300;
+
+        versionResource.SaveTo(path);
+    }
 }
